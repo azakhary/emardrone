6893cce1e8b6c482c7147938c8bee9fc62bdd17d
avetis <avetis.zakharyan@gmail.com>
2015-03-06 17:18:54 -0800

    also added reviews

--- stats_drone.php
+++ stats_drone.php
@@ -1,5 +1,7 @@
 <?php
 
+include "cUrl.php";
+
 function getPlusOne($package) {
     $main_url = "https://apis.google.com/u/0/se/3/_/+1/fastbutton?usegapi=1&annotation=inline&recommendations=false&size=medium&source=google%3Amarket&hl=en&origin=https%3A%2F%2Fplay.google.com&url=https%3A%2F%2Fmarket.android.com%2Fdetails%3Fid%3D".$package."&gsrc=3p&ic=1";
 
@@ -10,8 +12,24 @@ function getPlusOne($package) {
     return $count;
 }
 
+function getReviewCount($package) {
+    $url = "https://play.google.com/store/apps/details?id=$package&hl=en";
+
+    $html =  file_get_contents($url);
+
+    $count = cUrl::getBetweenString($html, 'stars-count"> (<span class="reviewers-small"></span>', ')');
+
+    $count = str_replace(',', '', $count);
+    
+    return $count;    
+}
+
 $package = "com.gamehivecorp.taptitans";
 
-$count = getPlusOne($package);
+$plus_one_count = getPlusOne($package);
+$review_count = getReviewCount($package);
+
+echo "Plus Ones: $plus_one_count \n";
+echo "Reviews: $review_count";
 
 ?>
